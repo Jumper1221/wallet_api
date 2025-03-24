656eea73c745438efdce0641f664b62533384e5b
Jumper1221 <62837752+Jumper1221@users.noreply.github.com>
2025-03-24 11:56:35 +0300

    reworked env and settings

--- app/settings.py
+++ app/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-w5cq1_qa%@cu#(=dpy^-%hywzcs0dxr=i=8^c^*gwg6ffqd6c0"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", False)
 
 ALLOWED_HOSTS = ["*"]
 
